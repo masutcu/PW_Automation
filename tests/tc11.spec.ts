a961cc78469a4130334f2a0e8340da861c627336
Mehmet Ali SÜTÇÜ <119762401+masutcu@users.noreply.github.com>
2024-04-05 16:11:54 +0300

    task12

--- tests/tc11.spec.ts
+++ tests/tc11.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test'
-//Test Case 10: Verify Subscription in home page
+//Test Case 11: Verify Subscription in Cart page
 //1. Launch browser
 //2. Navigate to url 'http://automationexercise.com'
 //3. Verify that home page is visible successfully
-//4. Scroll down to footer
-//5. Verify text 'SUBSCRIPTION'
-//6. Enter email address in input and click arrow button
-//7. Verify success message 'You have been successfully subscribed!' is visible
+//4. Click 'Cart' button
+//5. Scroll down to footer
+//6. Verify text 'SUBSCRIPTION'
+//7. Enter email address in input and click arrow button
+//8. Verify success message 'You have been successfully subscribed!' is visible
 
 
 let page; // Tarayıcı oturumunu burada tanımlıyoruz
@@ -30,24 +31,29 @@ test.describe('Test Case 10: Verify Subscription in home page', () => {
 
     });
 
-    test("Verify Subscription in home page", async () => {
-        //4. Scroll down to footer
-        
-        // Sayfa sonuna gitmek için JavaScript kodunu kullanıyoruz
-        await page.evaluate(() => {
-          window.scrollTo(0, document.body.scrollHeight); //tam dibi
-          //window.scrollTo(0, 500);//biraz -birsayfa- aşağı kaydırmak için
-        });
+    test("Verify Subscription in Cart page", async () => {
+        //4. Click 'Cart' button
+        await page.getByRole('link', { name: ' Cart ' }).click()
 
-        //5. Verify text 'SUBSCRIPTION'
+        //5. Scroll down to footer
+
+        //belirli bir alana kadar gitmek isteniyorsa
+        await page.getByPlaceholder('Your email address').scrollIntoViewIfNeeded();
+        /*
+                // Sayfa sonuna gitmek için JavaScript kodunu kullanıyoruz
+                await page.evaluate(() => {
+                  window.scrollTo(0, document.body.scrollHeight); //tam dibi
+                  //window.scrollTo(0, 500);//biraz -birsayfa- aşağı kaydırmak için
+                });
+        */
+        //6. Verify text 'SUBSCRIPTION'
         expect(await page.getByText('SUBSCRIPTION').isVisible()).toBeTruthy();
-        //6. Enter email address in input and click arrow button
+        //7. Enter email address in input and click arrow button
         await page.getByPlaceholder('Your email address').pressSequentially("testMS@gmail.com", { delay: 100 })
         await page.locator('#subscribe').click()
-        //7. Verify success message 'You have been successfully subscribed!' is visible
+        //8. Verify success message 'You have been successfully subscribed!' is visible
         expect(await page.getByText('successfully subscribed!').isVisible()).toBeTruthy();
 
-        
     })
 
 })
---
+++ tests/tc12.spec.ts
@@ -0,0 +1,94 @@
+import { test, expect } from '@playwright/test'
+//Test Case 12: Add Products in Cart
+//1. Launch browser
+//2. Navigate to url 'http://automationexercise.com'
+//3. Verify that home page is visible successfully
+//4. Click on 'Products' button
+//5. Hover over first product and click 'Add to cart'
+//6. Click 'Continue Shopping' button
+//7. Hover over second product and click 'Add to cart'
+//8. Click 'View Cart' button
+//9. Verify both products are added to Cart
+//10. Verify their prices, quantity and total price
+
+
+let page; // Tarayıcı oturumunu burada tanımlıyoruz
+test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage(); // Tarayıcı oturumunu başlatıyoruz
+});
+test.afterAll(async () => {
+    //await page.close();
+});
+
+
+test.describe('Test Case 12: Add Products in Cart', () => {
+
+    test("Navigate url", async () => {
+        //2. Navigate to url 'http://automationexercise.com'
+        //3. Verify that home page is visible successfully
+        await page.goto('/');
+        const title = await page.title();
+        console.log("Sayfa Başlığı:", title);
+        await expect(page).toHaveTitle(/Automation Exercise/);
+
+    });
+
+    test("Add Products in Cart", async () => {
+        //4. Click on 'Products' button      
+        await page.getByRole('link', { name: 'Products' }).click()
+        
+        //reklam
+        await page.goBack()
+        await page.goForward()
+        await page.waitForTimeout(2000)
+        
+        //5. Hover over first product and click 'Add to cart'
+        //6. Click 'Continue Shopping' button
+        await page.getByText('View Product').first().scrollIntoViewIfNeeded()
+        
+        await page.locator('.single-products').first().hover()
+        await page.locator('.add-to-cart').first().click()        
+        await page.getByRole('button', { name: 'Continue Shopping' }).click()
+        //7. Hover over second product and click 'Add to cart'
+        await page.locator('(//div[@class="single-products"])[2]').hover()
+        await page.getByText('Add to cart').nth(3).click()
+        //8. Click 'View Cart' button
+        await page.getByText('View Cart').click()
+        //9. Verify both products are added to Cart
+        const elements =await page.locator('.cart_description').count();
+        // Locator uzunluğunu kontrol et
+        expect(elements).toBe(2);
+        
+        //10. Verify their prices, quantity and total price
+        const price1= await page.locator('.cart_price').first().textContent()
+        const num1 = parseInt(price1.split(' ')[1]);
+        console.log('price1: ',num1); 
+
+        const price2= await page.locator('.cart_price').last().textContent()
+        const num2 = parseInt(price2.split(' ')[1]);
+        console.log('price2: ',num2); 
+        
+        const quantity1= await page.locator('.cart_quantity').first().textContent()
+        const qua1 = parseInt(quantity1);
+        console.log('quantity1: ',qua1);
+        
+        const quantity2= await page.locator('.disabled').last().textContent()
+        const qua2 = parseInt(quantity2);
+        console.log('quantity2: ',qua2); 
+
+        const total1= await page.locator('.cart_total_price').first().textContent()
+        const top1 = parseInt(total1.split(' ')[1]);
+        console.log('total1: ',top1); 
+
+        const total2= await page.locator('.cart_total_price').last().textContent()
+        const top2 = parseInt(total2.split(' ')[1]);
+        console.log('total2: ',top2); 
+        
+        
+        expect(top1).toEqual(num1*qua1)
+        expect(top2).toEqual(num2*qua2)
+
+
+    })
+
+})
