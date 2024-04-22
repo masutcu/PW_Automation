21e322893020fa3d395848c1ee1619fffb0ce8d5
Mehmet Ali SÜTÇÜ <119762401+masutcu@users.noreply.github.com>
2024-04-22 12:19:34 +0300

    taskCopmlated

--- tests/tc20.spec.ts
+++ tests/tc20.spec.ts
@@ -30,25 +30,25 @@ test.describe('Test Case 20: Search Products and Verify Cart After Login', () =>
     let ikinciUrun;
 
     test("Navigate url and Products Field", async () => {
-         //reklam - reklam çıkmadan önceki adımda eklenmelidir.
-         await page.route("**/*", route => {
+        //reklam - reklam çıkmadan önceki adımda eklenmelidir.
+        await page.route("**/*", route => {
             route.request().url().startsWith("https://googleads.") ?
                 route.abort() : route.continue();
             return;
         })
-        
+
         //2. Navigate to url 'http://automationexercise.com'       
         await page.goto('/');
-       
+
         await page.getByRole('link', { name: 'Products' }).click()
-       
+
 
 
 
     });
 
     test("Products - Search Input", async () => {
-       
+
         //4. Verify user is navigated to ALL PRODUCTS page successfully
         const title = await page.title();
         console.log("Sayfa Başlığı:", title);
@@ -56,15 +56,16 @@ test.describe('Test Case 20: Search Products and Verify Cart After Login', () =>
 
         //5. Enter product name in search input and click search button
         let aranan = "Jeans"
-        await page.getByPlaceholder('Search Product').pressSequentially(aranan, {delay:100})
-        
+        await page.getByPlaceholder('Search Product').pressSequentially(aranan, { delay: 100 })
+
         await page.locator('#submit_search').click()
-        
+
         //6. Verify 'SEARCHED PRODUCTS' is visible
         expect(await page.getByText('Searched Products').isVisible()).toBeTruthy()
 
         //7. Verify all the products related to search are visible
         const titleList = page.locator("div[class='single-products'] p")
+        //veya: page.$$("div[class='single-products'] p") ile locate alabiliriz.bu şekilde bir list döner ve loop içinde all() metodu kullanmaya gerek kalmaz.
         let found = false
         for (let title of await titleList.all()) {
             let kelime = await title.innerText()
@@ -77,13 +78,13 @@ test.describe('Test Case 20: Search Products and Verify Cart After Login', () =>
 
         //8. Add those products to cart
         //scroll
-        await page.evaluate(()=>{
-            window.scrollTo(0,500)
-          })
-        
-          //çıkan ürün sayısınca hepsini ekleyecek 
-          let numberOfElements = await page.locator("[class='productinfo text-center']").count()
-          console.log('Ürün sayısı: ',numberOfElements)
+        await page.evaluate(() => {
+            window.scrollTo(0, 500)
+        })
+
+        //çıkan ürün sayısınca hepsini ekleyecek 
+        let numberOfElements = await page.locator("[class='productinfo text-center']").count()
+        console.log('Ürün sayısı: ', numberOfElements)
 
 
         for (let num = 0; num < numberOfElements; num++) {
@@ -102,10 +103,10 @@ test.describe('Test Case 20: Search Products and Verify Cart After Login', () =>
         const title = await page.title();
         console.log("Sayfa Başlığı:", title);
         await expect(page).toHaveTitle(/Checkout/);
-        
+
         //ilk ürün görünürlüğü
         expect(await page.locator('td[class="cart_description"]').first().isVisible()).toBeTruthy();
-        ilkUrun=await page.locator('td[class="cart_description"]').first().textContent()
+        ilkUrun = await page.locator('td[class="cart_description"]').first().textContent()
         console.log('ilk ürün :', ilkUrun)
         expect(await page.locator('td[class="cart_description"]').last().isVisible()).toBeTruthy();
 
@@ -114,7 +115,7 @@ test.describe('Test Case 20: Search Products and Verify Cart After Login', () =>
     })
     test('Click on Login button', async () => {
         //10. Click 'Signup / Login' button and submit login details
-      
+
         await page.getByRole('link', { name: ' Login ' }).click()
         await expect(page.getByRole('heading', { name: "Login to your account" })).toBeVisible()
 
@@ -124,20 +125,20 @@ test.describe('Test Case 20: Search Products and Verify Cart After Login', () =>
         await singUpForm.getByPlaceholder('Email Address').pressSequentially(process.env.EMAIL, { delay: 100 })
         await singUpForm.getByPlaceholder('Password').fill(process.env.PSW)
         await singUpForm.getByRole('button', { name: 'Login' }).click()
-       
+
         //11. verify login and  go to Cart page
         await page.waitForTimeout(1000) //5 sn bekler
         await expect(page.getByText(' Logged in as ')).toBeVisible
         await expect(page.getByText(process.env.USERNAME)).toBeVisible
 
         await page.getByRole('link', { name: 'Cart' }).click()
-     
+
         //12. Verify that those products are visible in cart after login as well
         expect(await page.locator('td[class="cart_description"]').first().isVisible()).toBeTruthy();
-        ikinciUrun=await page.locator('td[class="cart_description"]').first().textContent()
+        ikinciUrun = await page.locator('td[class="cart_description"]').first().textContent()
         console.log('ikinci ilk ürün :', ikinciUrun)
-        expect( ilkUrun).toEqual(ikinciUrun);
-        
+        expect(ilkUrun).toEqual(ikinciUrun);
+
 
     })
 
---
+++ tests/tc25.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from '@playwright/test'
+//Test Case 25: Verify Scroll Up using 'Arrow' button and Scroll Down functionality
+//1. Launch browser
+//2. Navigate to url 'http://automationexercise.com'
+//3. Verify that home page is visible successfully
+//4. Scroll down page to bottom
+//5. Verify 'SUBSCRIPTION' is visible
+//6. Click on arrow at bottom right side to move upward
+//7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
+
+
+let page; // Tarayıcı oturumunu burada tanımlıyoruz
+test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage(); // Tarayıcı oturumunu başlatıyoruz
+});
+test.afterAll(async () => {
+    //await page.close();
+});
+
+
+test.describe('Test Case 25: Verify Scroll Up using Arrow button and Scroll Down functionality', () => {
+
+    test("Navigate url", async () => {
+        //2. Navigate to url 'http://automationexercise.com'
+        //3. Verify that home page is visible successfully
+        await page.goto('/');
+        const title = await page.title();
+        console.log("Sayfa Başlığı:", title);
+        await expect(page).toHaveTitle(/Automation Exercise/);
+
+    });
+
+    test("Verify Subscription in home page", async () => {
+        //4. Scroll down to buttom
+
+        // Sayfa sonuna gitmek için JavaScript kodunu kullanıyoruz
+        await page.evaluate(() => {
+            window.scrollTo(0, document.body.scrollHeight); //en dip
+            //window.scrollTo(0, 500);//biraz -birsayfa- aşağı kaydırmak için
+        });
+
+        //5. Verify text 'SUBSCRIPTION'
+        expect(await page.getByText('SUBSCRIPTION').isVisible()).toBeTruthy();
+
+        //6. Click on arrow at bottom right side to move upward
+        await page.locator('.fa-angle-up').click()
+
+        //7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
+        
+        expect(await page.locator('[class=col-sm-6] h2', {hastext:'Full-Fledged practice website for Automation Engineers'}).first().isVisible()).toBeTruthy();
+        await page.screenshot({ path: 'screenshot.png' });
+        await page.screenshot({ path: 'screenshotFullPage.png', fullPage: true });
+
+
+
+    })
+
+})
